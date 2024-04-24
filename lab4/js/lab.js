53d8537019db80c3c0b0f7f61308fc53912d2e5e
Daniel <Djgallegos7501@gmail.com>
2024-04-23 22:20:59 -0700

    Updated Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock, Paper, Scissars.
+// An implementation of the game.
 
-// Constants
+// Daniel Gallegos <djgalleg@ucsc.edu>
+// 24 April 2024
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Have two players.
+    // Designate a Player 1 and a Player 2
+// Winner of the game is decided by winning two out of three matches.
+// Each player has three tools to use.
+    // Rock, Paper, or Scissors
+// Players simultaneously choose one of the three choices on the count of three.
+// The winner of each mach is dependent on the choice of each player
+    // Rock wins against Scissors
+    // Scissors wins against Paper
+    // Paper wins against Rock
+// A tie will happen when each player makes the same choice.
+// When there is a tie the match will continue till one player beets the other.
