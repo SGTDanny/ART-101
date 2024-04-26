1048c31a6e7c8df16cca1d64b0d79d09d71d6037
Daniel <Djgallegos7501@gmail.com>
2024-04-25 18:49:40 -0700

    Made Lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+    *Lab 5: Data Typing and Variables
+    *Author: Daniel Gallegos
+    *Date: 26 April 2024
+*/
+
+// Define Variables
+var make = "Honda";
+var model = "Accord";
+var color = "Blue";
+var year = 2021;
+
+// Calculate
+var age = 2024 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
