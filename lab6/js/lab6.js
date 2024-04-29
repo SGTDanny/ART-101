5abd0e7f6dcd47d96768ce262a88d8eac4c3b682
Daniel <Djgallegos7501@gmail.com>
2024-04-28 20:25:03 -0700

    Began Lab 6

---
+++ lab6/js/lab6.js
@@ -0,0 +1,25 @@
+// Lab 6: JavaScript Arrays and Objects
+// Author: Daniel Gallegos
+// Date: 29 May 2024
+
+
+//Array
+const myTransportation = ["walking", "car", "TAPS Bus", "Metro Bus", "Uber"];
+
+//Object
+const myMainRide = {
+  make: "Honda",
+  model: "Accord",
+  color: "Blue",
+  year: 2021,
+  age: function() {
+    return 2024 - this.year;
+  }
+};
+
+//Output
+document.writeln("How I get around town:" + myTransport + "<br>");
+
+//Object Output
+document.writeln ("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
