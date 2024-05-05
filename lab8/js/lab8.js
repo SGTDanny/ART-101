87df22e1640edb6f9db85999bf30954ffe35be6e
Daniel <Djgallegos7501@gmail.com>
2024-05-05 12:24:28 -0700

    Task X complete

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -17,8 +17,14 @@ console.log("My array", array);
 var result = array.map(isEven);
 console.log ("Test of evennes of arrays:", result);
 
-var reslut = array.map(function(x){
+var result = array.map(function(x){
     return x ** 0.5;
 });
 
 console.log("Squareroot of array:", result);
+
+//Map reslut data
+var mapResults = "Original array: [100, 81, 4, 16, 42, 144, 10000]<br> Evenness of array:[true, false, true, true, true, true, true] <br> Squareroots of array:[10, 9, 2, 4, 6.48074069840786, 12, 100] ";
+
+//Set HTML content
+$("#output").html(mapResults);
